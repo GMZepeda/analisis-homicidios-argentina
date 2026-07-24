49b6b11fda7e08e3b4af3e3671db10a65b8fdbd6
Germán M. Zepeda <cienciadedatoszepeda@gmail.com>
2026-07-24 13:42:35 -0300

    Corrige código SQL y formatea ID de localidad con Python

--- creacion_tablas.sql
+++ creacion_tablas.sql
@@ -1,18 +1,17 @@
--- Creamos la tabla PROVINCIA
+-- 1. Tabla PROVINCIA
 CREATE TABLE provincia (
 	provincia_id INT PRIMARY KEY,
 	provincia_nombre VARCHAR(100)
 );
 
--- Creamos la tabla LOCALIDAD (depende de PROVINCIA)
+-- 2. Tabla LOCALIDAD
 CREATE TABLE localidad (
 	localidad_id INT PRIMARY KEY,
 	localidad_nombre VARCHAR(100),
-	prinvincia_id INT REFERENCES provincia(provincia_id)
-
+	provincia_id INT REFERENCES provincia(provincia_id)
 );
 
--- Creamos la tabla HECHO (depende de LOCALIDAD)
+-- 3. Tabla HECHO
 CREATE TABLE hecho (
 	hecho_id VARCHAR(50) PRIMARY KEY,
 	localidad_id INT REFERENCES localidad(localidad_id),
@@ -27,10 +26,10 @@ CREATE TABLE hecho (
 	motivo_origen_registro_otro VARCHAR(300)
 );
 
--- Creamos la tabla INVOLUCRADOS (depende de HECHO)
+-- 4. Tabla INVOLUCRADOS
 CREATE TABLE involucrados (
     involucrados_id SERIAL PRIMARY KEY,
-    tipo_involucrado VARCHAR(50), 
+    tipo_involucrado VARCHAR(50),
     sexo VARCHAR(50),
     identidad_genero VARCHAR(50),
     identidad_genero_otro VARCHAR(100),
@@ -38,6 +37,3 @@ CREATE TABLE involucrados (
     mayor_18_anios VARCHAR(50),
     hecho_id VARCHAR(50) REFERENCES hecho(hecho_id)
 );
-
--- se elimina para que coincida con el csv
-ALTER TABLE localidad DROP COLUMN prinvincia_id;
